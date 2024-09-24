fd4a8546741573efe38d99ec27321232e265a738
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-09-24 20:14:02 +0530

    Add files via upload

---
+++ where_clause.sql
@@ -0,0 +1,14 @@
+show databases;
+use my_shop;
+
+create table items3(
+item_id int auto_increment primary key,
+item_name varchar(50),
+item_price int,
+item_stock int NOT NULL
+);
+
+insert into items3(item_name,item_price,item_stock)values('pajamas','700',8);
+desc items3;
+select * from items;
+select * from items where item_name='hoddie';
